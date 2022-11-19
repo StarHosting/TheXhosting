6dbc51d96b414a0bf2a1df2f8eb01d25e87c6d6d
Jesus Garcia A <40673956+garciaqjesus@users.noreply.github.com>
2022-11-19 02:06:50 +0100

    Delete PanelFiles directory

--- PanelFiles/resources/scripts/api/server/playermanager/getPlayers.ts
+++
@@ -1,8 +0,0 @@
-import http from '@/api/http';
-import { PlayerManagerResponse } from '@/components/server/playermanager/PlayerManagerContainer';
-
-export default async (uuid: string): Promise<PlayerManagerResponse> => {
-    const { data } = await http.get(`/api/client/servers/${uuid}/playermanager`, {});
-
-    return (data.data || []);
-};
